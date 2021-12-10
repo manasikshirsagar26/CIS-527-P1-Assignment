d2ece35f51c11bd3c9abf549a89c914c85300c48
Manasi Kshirsagar <manasikshirsagar44@gmail.com>
2021-12-10 16:54:27 -0500

    updated user interface

--- P1_Client.py
+++ P1_Client.py
@@ -13,43 +13,45 @@ import sys
 
 SERVER_IP = sys.argv[1] #'127.0.0.1'  # The server's hostname or IP address
 print("server ip provided", SERVER_IP)
-SERVER_PORT = 14807        # Server port
+SERVER_PORT = 4807        # Server port
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP,SERVER_PORT))
+    s.connect((SERVER_IP,SERVER_PORT))  # Connection established between client and server
     while(True):
         print('\n')
         client_input = input('Please provide input MSGGET or MSGSTORE or EXIT: ')
-        if (client_input == 'MSGGET'):
+        if (client_input == 'MSGGET'):           # MSGGET Command
+            client_input = client_input +'\n'
             print('c: MSGGET')
             s.sendall(client_input.encode())
             receivedData = s.recv(1024)
             receivedData = receivedData.decode()
-            print('s: '+receivedData)
-            # print(str(repr('s:'+receivedData)))
-        elif (client_input == 'MSGSTORE'):
+            print('s: '+ receivedData)
+        elif (client_input == 'MSGSTORE'):      # MSGSTORE Command
+            client_input = client_input +'\n'
+            print(client_input)
             print('c: MSGSTORE')
             s.sendall(client_input.encode())
-            # print('Client request msg store')
             receivedData = s.recv(1024)
             receivedData = receivedData.decode()
-            # print('Data is :'+ receivedData)
-            # print('ack received from server')
+            #print("received:",receivedData)
             if (receivedData == '200 OK'):
-                # print(repr(receivedData))
                 print('s: '+receivedData)
                 message_of_the_day = 'Imagination is more important than knowledge.'
+                print('Default input message of the day is: ', message_of_the_day)
+                user_input_message_of_the_day = input('Please provide message of the day: ')
+                if (user_input_message_of_the_day != ''):
+                    message_of_the_day = user_input_message_of_the_day
                 print('c: '+ message_of_the_day)
                 s.sendall(message_of_the_day.encode())
                 receivedData = s.recv(1024)
                 receivedData = receivedData.decode()
                 print('s: '+receivedData)
-                # print(repr(receivedData))
-        elif (client_input == 'EXIT'):
+        elif (client_input == 'EXIT' or client_input == 'exit' or client_input =='Exit'):  # Exit the program
             print('Exit requested by user. Exiting the program.')
             break
         else:
-            print('Unrecognised Input, Please provide input MSGGET or MSGSTORE')
-            break
+            print('Unrecognised Input, Please try again') # Unrecognised input 
+            continue
         
 
--- P1_Server.py
+++ P1_Server.py
@@ -11,41 +11,50 @@
 
 import socket
 
+#############################################################################
+################     Disclaimer ################
+###### Following section of code finds IP address automatically 
+#       when this file runs on any computer(connected to internet)
+#       However, port 80 might be busy, in that case, 
+#       this file when run will produce errors on terminal.
+#       In that casem Kindly comment following section and hardcode appropriate server IP below. 
 # Find server IP address
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print("Server IP: ", s.getsockname()[0]) # Server IP address
 SERVER_IP = s.getsockname()[0]
 s.close()
+################    End of Disclaimer   ################
+#############################################################################
 
-SERVER_IP = '192.168.0.11'
+# Configure Server IP below
+# SERVER_IP = '192.168.0.11'
+
 # Define server port using UMID
-SERVER_PORT = 14807
+SERVER_PORT = 4807
 acknowledgment = '200 OK'       # Intializing the ACK signal value
 message_of_the_day = 'Anyone who has never made a mistake has never tried anything new.'  # Initializing Message of the day value                                     
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((SERVER_IP, SERVER_PORT))                                   # Associate the socket with a specific network interface and port number
+    s.bind((SERVER_IP, SERVER_PORT))                                # Associate the socket with a specific network interface and port number
     s.listen()                                                      # listens for connections from client
     conn, addr = s.accept()                                         # to establish a connection to the server and initiate the three-way handshake
     with conn:
-        print('Connected with client IP:', addr)
         while True:
             receivedData = conn.recv(1024)                          
             receivedData = receivedData.decode()
+            print(receivedData)
             if not receivedData:
                 print('No data received from client. Exiting program.\n')
                 break
-            elif (receivedData == 'MSGGET'):
-                sending = acknowledgment +'\n'+ message_of_the_day + '\n'
+            elif (receivedData == 'MSGGET\n'):
+                sending = acknowledgment +'\n   '+ message_of_the_day + '\n'
                 conn.sendall(sending.encode())
-            elif (receivedData == 'MSGSTORE'):
-                #print('This is msg store\n')
+            elif (receivedData == 'MSGSTORE\n'):
+                #print('This is msg store\n',receivedData)
                 conn.sendall(acknowledgment.encode())
                 receivedData = conn.recv(1024)
                 receivedData = receivedData.decode()
                 message_of_the_day = receivedData
-                # print('Updated : message_of_the_day: ' + message_of_the_day)
-                conn.sendall(acknowledgment.encode())
-            #print(data)
-            #print(type(data))
+                conn.sendall((acknowledgment+'\n').encode())
+
             
