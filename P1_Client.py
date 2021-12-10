4fd66fbd5dd025c4182139d8fab0fd2d20174c1d
Manasi Kshirsagar <manasikshirsagar44@gmail.com>
2021-12-10 00:04:18 -0500

    Modified for MSGSTORE and MSGGET.

--- P1_Client.py
+++ P1_Client.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+#######################################################
+# CIS 527: Computer Networks
+# Name: Manasi Kshirsagar
+# UMID- 05494807
+# Instructor: Prof. Zheng Song
+# Semester: Fall 2021 
+# P1 assignment: Socket Programming - Client Side
+#######################################################
 
 import socket
 
@@ -7,24 +15,32 @@ SERVER_PORT = 4807        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((CLIENT, SERVER_PORT))
-    client_input = input('Please provide input MSGGET or MSGSTORE: ')
+    client_input = input('Please provide input MSGGET or MSGSTORE or EXIT: ')
     if (client_input == 'MSGGET'):
+        print('c: MSGGET')
         s.sendall(client_input.encode())
-        receivedData = s.recv(1024)
-        print(repr(receivedData))
-    elif (client_input == 'MSGSTORE'):
-        s.sendall(client_input.encode())
-        print('Client request msg store')
         receivedData = s.recv(1024)
         receivedData = receivedData.decode()
-        print('Data is :'+ receivedData)
-        print('ack received from server')
+        print('s: '+receivedData)
+        # print(str(repr('s:'+receivedData)))
+    elif (client_input == 'MSGSTORE'):
+        print('c: MSGSTORE')
+        s.sendall(client_input.encode())
+        # print('Client request msg store')
+        receivedData = s.recv(1024)
+        receivedData = receivedData.decode()
+        # print('Data is :'+ receivedData)
+        # print('ack received from server')
         if (receivedData == '200 OK'):
-            print(repr(receivedData))
+            # print(repr(receivedData))
+            print('s: '+receivedData)
             message_of_the_day = 'Imagination is more important than knowledge.'
+            print('c: '+ message_of_the_day)
             s.sendall(message_of_the_day.encode())
             receivedData = s.recv(1024)
-            print(repr(receivedData))
+            receivedData = receivedData.decode()
+            print('s: '+receivedData)
+            # print(repr(receivedData))
     else:
         print('Unrecognised Input, Please provide input MSGGET or MSGSTORE')
     
--- P1_Server.py
+++ P1_Server.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+#######################################################
+# CIS 527: Computer Networks
+# Name: Manasi Kshirsagar
+# UMID- 05494807
+# Instructor: Prof. Zheng Song
+# Semester: Fall 2021 
+# P1 assignment: Socket Programming - Server Side
+#######################################################
 
 import socket
 
@@ -18,17 +26,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not receivedData:
                 break
             elif (receivedData == 'MSGGET'):
-                sending = acknowledgment + '\n' + message_of_the_day + '\n'
+                sending = acknowledgment +'\n'+ message_of_the_day + '\n'
                 conn.sendall(sending.encode())
             elif (receivedData == 'MSGSTORE'):
-                print('This is msg store\n')
+                #print('This is msg store\n')
                 conn.sendall(acknowledgment.encode())
                 receivedData = conn.recv(1024)
                 receivedData = receivedData.decode()
                 message_of_the_day = receivedData
-                print('Updated : message_of_the_day: ' + message_of_the_day)
+                # print('Updated : message_of_the_day: ' + message_of_the_day)
                 conn.sendall(acknowledgment.encode())
             #print(data)
             #print(type(data))
-            
             
