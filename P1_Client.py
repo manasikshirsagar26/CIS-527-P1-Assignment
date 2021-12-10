078350d1dcc7dfb555d6273e76e051c137b70051
Manasi Kshirsagar <manasikshirsagar44@gmail.com>
2021-12-10 15:19:03 -0500

    Added looping on client and server side. Functional code.

--- P1_Client.py
+++ P1_Client.py
@@ -13,39 +13,43 @@ import sys
 
 SERVER_IP = sys.argv[1] #'127.0.0.1'  # The server's hostname or IP address
 print("server ip provided", SERVER_IP)
-SERVER_PORT = 4807        # Server port 
+SERVER_PORT = 14807        # Server port
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP, SERVER_PORT))
-    client_input = input('Please provide input MSGGET or MSGSTORE or EXIT: ')
-    if (client_input == 'MSGGET'):
-        print('c: MSGGET')
-        s.sendall(client_input.encode())
-        receivedData = s.recv(1024)
-        receivedData = receivedData.decode()
-        print('s: '+receivedData)
-        # print(str(repr('s:'+receivedData)))
-    elif (client_input == 'MSGSTORE'):
-        print('c: MSGSTORE')
-        s.sendall(client_input.encode())
-        # print('Client request msg store')
-        receivedData = s.recv(1024)
-        receivedData = receivedData.decode()
-        # print('Data is :'+ receivedData)
-        # print('ack received from server')
-        if (receivedData == '200 OK'):
-            # print(repr(receivedData))
-            print('s: '+receivedData)
-            message_of_the_day = 'Imagination is more important than knowledge.'
-            print('c: '+ message_of_the_day)
-            s.sendall(message_of_the_day.encode())
+    s.connect((SERVER_IP,SERVER_PORT))
+    while(True):
+        print('\n')
+        client_input = input('Please provide input MSGGET or MSGSTORE or EXIT: ')
+        if (client_input == 'MSGGET'):
+            print('c: MSGGET')
+            s.sendall(client_input.encode())
             receivedData = s.recv(1024)
             receivedData = receivedData.decode()
             print('s: '+receivedData)
-            # print(repr(receivedData))
-    elif (client_input == 'EXIT'):
-        print('Exit requested by user. Exiting the program.')
-    else:
-        print('Unrecognised Input, Please provide input MSGGET or MSGSTORE')
-    
+            # print(str(repr('s:'+receivedData)))
+        elif (client_input == 'MSGSTORE'):
+            print('c: MSGSTORE')
+            s.sendall(client_input.encode())
+            # print('Client request msg store')
+            receivedData = s.recv(1024)
+            receivedData = receivedData.decode()
+            # print('Data is :'+ receivedData)
+            # print('ack received from server')
+            if (receivedData == '200 OK'):
+                # print(repr(receivedData))
+                print('s: '+receivedData)
+                message_of_the_day = 'Imagination is more important than knowledge.'
+                print('c: '+ message_of_the_day)
+                s.sendall(message_of_the_day.encode())
+                receivedData = s.recv(1024)
+                receivedData = receivedData.decode()
+                print('s: '+receivedData)
+                # print(repr(receivedData))
+        elif (client_input == 'EXIT'):
+            print('Exit requested by user. Exiting the program.')
+            break
+        else:
+            print('Unrecognised Input, Please provide input MSGGET or MSGSTORE')
+            break
+        
 
--- P1_Server.py
+++ P1_Server.py
@@ -10,28 +10,29 @@
 
 import socket
 
+# Find server IP address
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# print("Server IP: ", s.getsockname()[0]) # Server IP address
+# SERVER_IP = s.getsockname()[0]
+# s.close()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
-SERVER_IP = s.getsockname()[0]
-s.close()
-
-# CLIENT = '192.168.0.11'                                            # The server's hostname or IP address
-SERVER_PORT = 4807                                                  # Server port 
-acknowledgment = '200 OK'                                           # Intializing the ACK signal value
+SERVER_IP = '192.168.0.11'
+# Define server port using UMID
+SERVER_PORT = 14807
+acknowledgment = '200 OK'       # Intializing the ACK signal value
 message_of_the_day = 'Anyone who has never made a mistake has never tried anything new.'  # Initializing Message of the day value                                     
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((SERVER_IP, SERVER_PORT))                                   # Associate the socket with a specific network interface and port number
     s.listen()                                                      # listens for connections from client
     conn, addr = s.accept()                                         # to establish a connection to the server and initiate the three-way handshake
     with conn:
-        # print('Connected by', addr)
+        print('Connected with client IP:', addr)
         while True:
             receivedData = conn.recv(1024)                          
             receivedData = receivedData.decode()
             if not receivedData:
-                print('Not data.')
+                print('No data received from client. Exiting program.\n')
                 break
             elif (receivedData == 'MSGGET'):
                 sending = acknowledgment +'\n'+ message_of_the_day + '\n'
